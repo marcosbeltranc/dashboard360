415c119e4eb8c6ae43f285206344dc13c09b55aa
marcosbeltranc <mabc181@hotmail.com>
2026-05-13 10:01:52 -0600

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://gaiabackend.grupomepiel.com.mx/api',
     // baseURL: 'http://172.16.101.119:8000/api',
 });
 
